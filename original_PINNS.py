1012cccb6b016478209db0577e90fe64787c0f43
Charlie-R10 <108882229+Charlie-R10@users.noreply.github.com>
2023-11-28 15:11:04 +0000

    Updated original_PINNS.py
    Added function to display parameters such as r2^2 etc.
    Also added scalable parameter to loss function based on loss study

--- original_PINNS.py
+++ original_PINNS.py
@@ -2,15 +2,16 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.python.ops.numpy_ops import np_config
-np_config.enable_numpy_behavior()
 import os
 import math
+np_config.enable_numpy_behavior()
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 
 # Want to review the layers and also activation function
 
-
-# Defining layer
+# Code to define model architecture
 def diffusion_PINN():
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(20, activation='tanh', input_shape=(1,))
@@ -31,17 +32,17 @@ model = diffusion_PINN()
 normal_neural_net = diffusion_PINN()
 
 
+# Loss function for physics informed loss
+def pinns_loss():
 
-# Loss function
-def pinns_loss(y_true, y_pred):
-    # Coefficients/constants (change if necessary) - maybe should be defined globally
+    # Coefficients/constants
     D = 1.0  # Diffusion coefficient
     sigma_a = 0.5  # Absorption cross-section
     nu_Sigma_f = 0.2  # Fission-related term
     S = 2.0  # Source term - started with no net neutron source
-    L = D/sigma_a
+    L = D / sigma_a  # L term (from book)
 
-    x = tf.constant(np.linspace(0, a, 100), dtype=tf.float32)
+    x = tf.constant(np.linspace(0, a, 100), dtype=tf.float32) # Linearly space 100 points over domain
 
     # Compute the first and the second derivatives of phi_pred with respect to x
     with tf.GradientTape() as t1:
@@ -52,49 +53,50 @@ def pinns_loss(y_true, y_pred):
         dphi_dx = t2.gradient(phi_pred, x)
     d2phi_dx2 = t1.gradient(dphi_dx, x)
 
-
     # Residual loss (from book)
-    L_residual = (d2phi_dx2 - (1/L**2)* phi_pred) # rest of eq:  - nu_Sigma_f * phi_pred - S
-    # Boundary condition loss
+    L_residual = (d2phi_dx2 - (1 / L ** 2) * phi_pred)  # rest of eq:  - nu_Sigma_f * phi_pred - S
+
+    # Boundary condition losses
 
     # Boundary condition J(a) = 0
     L_residual += dphi_dx[-1]
 
+    # Boundary condition phi(a) = 0
     L_residual += phi_pred[-1]
-
-    # J = derivative of phi (current)
-    # This is boundary condition J+=1/2*S
-    #L_residual += (dphi_dx[0] - 0.5 * S)
 
     # MSE of residual
     mse_residual = tf.reduce_mean(tf.square(L_residual))
 
     return mse_residual
 
+
+# Traditional loss (MAE same as normal neural net)
 def traditional_loss(y_true, y_pred):
     return tf.keras.losses.MeanSquaredError()(y_true, y_pred)
 
+
+# Combined loss of PINNS and NN
 def combined_loss(y_true, y_pred):
     trad_loss = traditional_loss(y_true, y_pred)
-    pinn_loss_val = pinns_loss(y_true, y_pred)  # Make sure to pass the necessary inputs here
-    total_loss = trad_loss + pinn_loss_val
+    pinn_loss_val = pinns_loss()
+    total_loss = 5*trad_loss + pinn_loss_val # '5' is a scalable parameter - can change this to alter loss function
     return total_loss
+
 
 # Compile model
 model.compile(loss=combined_loss, optimizer='adam', run_eagerly=True)
 normal_neural_net.compile(loss=['MeanAbsoluteError'], optimizer='adam')
 
-
 # Define parameters for the problem
-D = 1.0           # Diffusion coefficient
-sigma_a = 0.5     # Absorption cross-section
+D = 1.0  # Diffusion coefficient
+sigma_a = 0.5  # Absorption cross-section
 nu_Sigma_f = 0.2  # Fission-related term
-S = 2.0           # Source term - set as 2 for now
-a = 10.0          # Length of the domain
-L = math.sqrt(D/sigma_a) # L^2 = D/sigma_a
+S = 2.0  # Source term - set as 2 for now
+a = 10.0  # Length of the domain
+L = math.sqrt(D / sigma_a)  # L^2 = D/sigma_a
 
-# x is number of points for data (10 evenly spaced 1m apart for now)
-num_points = 2
+# x is number of points for data and spaced over a certain part
+num_points = 4
 x = np.linspace(0, 2, num_points)
 x_val_points = np.linspace(0, a, 100)
 
@@ -102,41 +104,49 @@ x_val_points = np.linspace(0, a, 100)
 # Generate training data
 def generate_training_data(x):
     # Calculate the true neutron flux based on the diffusion equation pg 51 in Stacey
-    #phi_true = 4*S*((np.sinh(a-x)/L)+(2*D/L)*np.cosh(a-x)/L)/((((2*(D/L)+1)**2)*np.exp(a/L))-((2*(D/L)-1)**2)*np.exp(-a/L))
-    phi_true = ((L*S)/(2*D))*np.exp(-x/L)
+    phi_true = ((L * S) / (2 * D)) * np.exp(-x / L)
     return x, phi_true
 
 
+# Train the model
 x_train, phi_train_true = generate_training_data(x)
 x_test, phi_test = generate_training_data(x_val_points)
 
-
-
-# Train the model
-num_epochs = 200  # YChange if necessary ~ did 1000 took roughly 1.5 hrs set as 10 for now
+num_epochs = 2000  # Change if necessary
 batch_size = 32
 
+
+# Generate data for both models
 history = model.fit(x_train, phi_train_true, epochs=num_epochs, batch_size=batch_size)
 history_nn = normal_neural_net.fit(x_train, phi_train_true, epochs=num_epochs, batch_size=batch_size)
 
 
-# Plot loss - MAE of ~ 3x10^-5
-# Want to look at r2 value next
-
-def loss_plotter(history):
-    plt.plot(history.history['loss'])
-    plt.title('Model Loss over Time')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss (MSE)')
-    plt.show()
-
-
-# Evaluate with test data
+# Plot analytical solution, traditional NN and PINN
 plt.plot(x_test, phi_test, label='Analytical solution')
 plt.plot(x_test, model.predict(x_test), label='PINN')
 plt.plot(x_test, normal_neural_net.predict(x_test), label='Traditional NN')
 plt.legend()
 plt.show()
+
+
+predicted_pinn = model.predict(x_test)
+predicted_nn = normal_neural_net.predict(x_test)
+
+
+# Function to display r^2, RMSE, MAE, max error and max % error for model
+def model_metrics(values):
+    predicted_data = np.array(values, dtype=np.float32)
+    y_true = np.array(phi_test, dtype=np.float32)
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, max_error, r2_score
+    print(f'r^2 (PINN) = {r2_score(y_true, predicted_data)}')
+    print(f'RMSE (PINN) = {np.sqrt(mean_squared_error(y_true, predicted_data))}')
+    print(f'MAE (PINN) = {mean_absolute_error(y_true, predicted_data)}')
+    print(f'MAE PERCENTAGE (PINN) = {mean_absolute_percentage_error(y_true, predicted_data) * 100000}')
+    print(f'MAX ERROR (PINN) = {max_error(y_true, predicted_data) * 100000}')
+
+
+model_metrics(predicted_pinn)
+model_metrics(predicted_nn)
 
 
 
